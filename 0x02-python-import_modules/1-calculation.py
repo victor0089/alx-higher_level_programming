82fa6fce6f62d61500fde7507f553a1469db41d5
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-10 23:41:43 +0300

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Author - Victor Israel
+
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
