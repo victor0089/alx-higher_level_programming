3a727ec43d85e4eba1bb0505987e220d0d85aaaf
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-11 20:52:52 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 
 def no_c(my_string):
-    new_string = ""
+    new_string = ''
     for character in my_string:
         if character != 'c' and character != 'C':
             new_string += character
