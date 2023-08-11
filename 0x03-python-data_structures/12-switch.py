e44680f6a12264815d508737ac97462ef9ffbd98
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-08-11 20:50:33 +0300

    Update 12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 a = 89
 b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
+(a, b) = (b, a)
+print 'a={:d} - b={:d}'.format(a, b)
