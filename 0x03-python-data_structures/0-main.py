f020e1ce4a435b3318b6cd821a84e86bb49b0d55
victor <victor.mecdoors@gmail.com>
2023-09-11 11:50:42 -0700

    0x0A-python-inheritance by victor

--- 0x03-python-data_structures/0-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-print_list_integer = __import__('0-print_list_integer'
-                                ).print_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-higher_level_programming
+# alx-higher_level_programming0x0A-python-inheritance by victor
