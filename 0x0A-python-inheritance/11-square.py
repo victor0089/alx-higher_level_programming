df6b4c8c81a48856551f1519835132292b6b6922
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-09-12 00:47:17 +0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,39 @@
- a Rectangle subclass Square."""
+#!/usr/bin/python3
+"""
+Module 10-square
+
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
+
+Contains subclass Square
+with instantiation of private attribute size, validated by superclass,
+and prints with __str__
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """inherits from Rectangle, who inherits from BaseGeometry
+    methods:
+        __init__(self, size)
+        __str__(self)
+    """
     def __init__(self, size):
-        """Initialize a new square.
-
+        """initializes size
         Args:
-            size (int): The size of the new square.
+            size (int): private
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
