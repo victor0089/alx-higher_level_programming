a754bcceecf3b307bf4d62669361a59ded5fe668
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-03 23:40:25 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@
 """
 
 
-if __name__ == "__main__": )
+if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+        content=res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
