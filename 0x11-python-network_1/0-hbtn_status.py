d97187bc63f666f423507e43a2720fabf54bf19b
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-12-03 23:04:41 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,6 @@
 - uses urlib package
 """
 
-
 if __name__ == '__main__':
     import urllib.request
 
@@ -14,3 +13,4 @@ if __name__ == '__main__':
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        
